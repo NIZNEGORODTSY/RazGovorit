714582015d1e0a4c448e0c36152c42dc631524c3
ChurinKirill <kirillchurin07@mail.ru>
2025-04-10 08:37:27 +0300

    test

--- backend/app.py
+++ backend/app.py
@@ -12,5 +12,11 @@ def simplify():
         "result": modelapi.simplify_text(text)
     })
 
+@app.route("/test", methods=["POST"])
+def test():
+    return jsonify({
+        "result": "testresult"
+    })
+
 
 app.run(debug=False, host="0.0.0.0", port=5000)
