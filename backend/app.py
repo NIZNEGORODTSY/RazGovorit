8d7aee135298e4a559f07bdc05d9bba1008b9b2c
Kirill Churin <kirillchurin07@mail.ru>
2025-05-19 18:35:25 +0300

    готова обработка файлов

--- backend/app.py
+++ backend/app.py
@@ -83,7 +83,4 @@ def process_file():
     else:
         return jsonify({ 'error': 'Filetype is not allowed' }), 400
 
-if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=5000)
-else:
-    app.run(debug=False, host="0.0.0.0", port=5000)
+app.run(debug=False, host="0.0.0.0", port=5000)
