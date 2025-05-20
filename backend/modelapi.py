ee5c11ac30b1940ea2c2c914743fc4e1a39df17b
Kirill Churin <kirillchurin07@mail.ru>
2025-05-20 17:39:37 +0300

    Интегрирована русскоязычная модель, налажено взаимодействие с ней

--- backend/modelapi.py
+++ backend/modelapi.py
@@ -1,24 +1,35 @@
-from transformers import T5ForConditionalGeneration, T5Tokenizer
+# from transformers import T5ForConditionalGeneration, T5Tokenizer, AutoTokenizer
+from transformers import AutoTokenizer
 import torch
 import os
+import joblib
 
-# Загрузка модели и токенизатора
+model_name = "IlyaGusev/rut5_base_sum_gazeta"
+tokenizer = AutoTokenizer.from_pretrained(model_name)
 
-model_path = os.path.join(os.path.dirname(__file__), "my_model/")
+model_path = os.path.join(os.path.dirname(__file__), "my_model/cpu_model.pkl")
+model = joblib.load(model_path)
+# model = model.to('cpu')
 
-t = os.path.exists(model_path)
+# model = torch.load(model_path, map_location=torch.device("cpu"))
 
-tokenizer = T5Tokenizer.from_pretrained(
-    model_path,
-    tokenizer_file=os.path.join(model_path, "/spiece.model")
-)
+# Загрузка модели и токенизатора (старая модель)
 
-model = T5ForConditionalGeneration.from_pretrained(
-    model_path,
-    torch_dtype=torch.float32,
-    device_map="cpu",
-    use_safetensors=True
-)
+# model_path = os.path.join(os.path.dirname(__file__), "my_model/")
+
+# t = os.path.exists(model_path)
+
+# tokenizer = T5Tokenizer.from_pretrained(
+#     model_path,
+#     tokenizer_file=os.path.join(model_path, "/spiece.model")
+# )
+
+# model = T5ForConditionalGeneration.from_pretrained(
+#     model_path,
+#     torch_dtype=torch.float32,
+#     device_map="cpu",
+#     use_safetensors=True
+# )
 
 def postprocess_text(text: str) -> str:
     i = 0
@@ -29,31 +40,71 @@ def postprocess_text(text: str) -> str:
 
 def simplify_text(text: str) -> str:
 
-    inputs = tokenizer("summarize, simplify and keep the main meaning |" + text, return_tensors="pt", max_length=512, truncation=True).to("cpu")
+    # inputs = tokenizer("summarize, simplify and keep the main meaning |" + text, return_tensors="pt", max_length=512, truncation=True).to("cpu")
 
-    # outputs = model.generate(inputs["input_ids"], max_length=150, min_length=50, length_penalty=2.0, num_beams=5, early_stopping=True)
+    # # outputs = model.generate(inputs["input_ids"], max_length=150, min_length=50, length_penalty=2.0, num_beams=5, early_stopping=True)
     
-    outputs = model.generate(
-        inputs.input_ids,
-        max_length=1000,
-        min_length=10,
-        num_beams=4,
-        early_stopping=True
-    )
+    # outputs = model.generate(
+    #     inputs.input_ids,
+    #     max_length=1000,
+    #     min_length=10,
+    #     num_beams=4,
+    #     early_stopping=True
+    # )
 
-    summary = tokenizer.decode(outputs[0], skip_special_tokens=True)
+    # summary = tokenizer.decode(outputs[0], skip_special_tokens=True)
+    input_ids = tokenizer(
+        ["Упрости и перефразируй текст: " + text],
+        max_length=600,
+        add_special_tokens=True,
+        padding="max_length",
+        truncation=True,
+        return_tensors="pt"
+    )["input_ids"]
+
+    output_ids = model.generate(
+        input_ids=input_ids,
+        no_repeat_ngram_size=4,
+        num_beams = 10,
+        length_penalty = 0.5,
+        max_new_tokens=200,
+        min_new_tokens=30,
+    )[0]
+
+    summary = tokenizer.decode(output_ids, skip_special_tokens=True)
     return postprocess_text(summary)
 
 def clearize_text(text: str) -> str:
-    inputs = tokenizer("summarize, simplify and keep the main meaning |" + text, return_tensors="pt", max_length=512, truncation=True).to("cpu")
+    # inputs = tokenizer("summarize, simplify and keep the main meaning |" + text, return_tensors="pt", max_length=512, truncation=True).to("cpu")
     
-    outputs = model.generate(
-        inputs.inputs_ids,
-        max_length=250,
-        min_length=100,
-        num_beams=4,
-        early_stopping=True
-    )
+    # outputs = model.generate(
+    #     inputs.inputs_ids,
+    #     max_length=250,
+    #     min_length=100,
+    #     num_beams=4,
+    #     early_stopping=True
+    # )
 
-    summary = tokenizer.decode(outputs[0], skip_special_tokens=True)
+    # summary = tokenizer.decode(outputs[0], skip_special_tokens=True)
+
+    # Временно для ясного текста используется тот же запрос
+    input_ids = tokenizer(
+        ["Упрости и перефразируй текст: " + text],
+        max_length=600,
+        add_special_tokens=True,
+        padding="max_length",
+        truncation=True,
+        return_tensors="pt"
+    )["input_ids"]
+
+    output_ids = model.generate(
+        input_ids=input_ids,
+        no_repeat_ngram_size=4,
+        num_beams = 10,
+        length_penalty = 0.5,
+        max_new_tokens=200,
+        min_new_tokens=30,
+    )[0]
+
+    summary = tokenizer.decode(output_ids, skip_special_tokens=True)
     return postprocess_text(summary)
