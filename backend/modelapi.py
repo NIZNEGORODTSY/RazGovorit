35bbb980aa54c417dbd0e1f59c1f3b16a0813d60
Kirill Churin <kirillchurin07@mail.ru>
2025-05-21 12:37:20 +0300

    финальная модель + чистка от старого кода

--- backend/modelapi.py
+++ backend/modelapi.py
@@ -1,4 +1,3 @@
-# from transformers import T5ForConditionalGeneration, T5Tokenizer, AutoTokenizer
 from transformers import AutoTokenizer
 import torch
 import os
@@ -7,29 +6,8 @@ import joblib
 model_name = "IlyaGusev/rut5_base_sum_gazeta"
 tokenizer = AutoTokenizer.from_pretrained(model_name)
 
-model_path = os.path.join(os.path.dirname(__file__), "my_model/cpu_model.pkl")
+model_path = os.path.join(os.path.dirname(__file__), "my_model/new_best_model.pkl")
 model = joblib.load(model_path)
-# model = model.to('cpu')
-
-# model = torch.load(model_path, map_location=torch.device("cpu"))
-
-# Загрузка модели и токенизатора (старая модель)
-
-# model_path = os.path.join(os.path.dirname(__file__), "my_model/")
-
-# t = os.path.exists(model_path)
-
-# tokenizer = T5Tokenizer.from_pretrained(
-#     model_path,
-#     tokenizer_file=os.path.join(model_path, "/spiece.model")
-# )
-
-# model = T5ForConditionalGeneration.from_pretrained(
-#     model_path,
-#     torch_dtype=torch.float32,
-#     device_map="cpu",
-#     use_safetensors=True
-# )
 
 def postprocess_text(text: str) -> str:
     i = 0
@@ -39,22 +17,8 @@ def postprocess_text(text: str) -> str:
     return text
 
 def simplify_text(text: str) -> str:
-
-    # inputs = tokenizer("summarize, simplify and keep the main meaning |" + text, return_tensors="pt", max_length=512, truncation=True).to("cpu")
-
-    # # outputs = model.generate(inputs["input_ids"], max_length=150, min_length=50, length_penalty=2.0, num_beams=5, early_stopping=True)
-    
-    # outputs = model.generate(
-    #     inputs.input_ids,
-    #     max_length=1000,
-    #     min_length=10,
-    #     num_beams=4,
-    #     early_stopping=True
-    # )
-
-    # summary = tokenizer.decode(outputs[0], skip_special_tokens=True)
     input_ids = tokenizer(
-        ["Упрости и перефразируй текст и сохрани основное значение текста: " + text],
+        [text],
         max_length=600,
         add_special_tokens=True,
         padding="max_length",
@@ -74,22 +38,10 @@ def simplify_text(text: str) -> str:
     summary = tokenizer.decode(output_ids, skip_special_tokens=True)
     return postprocess_text(summary)
 
+#  Ясный текст в разработке
 def clearize_text(text: str) -> str:
-    # inputs = tokenizer("summarize, simplify and keep the main meaning |" + text, return_tensors="pt", max_length=512, truncation=True).to("cpu")
-    
-    # outputs = model.generate(
-    #     inputs.inputs_ids,
-    #     max_length=250,
-    #     min_length=100,
-    #     num_beams=4,
-    #     early_stopping=True
-    # )
-
-    # summary = tokenizer.decode(outputs[0], skip_special_tokens=True)
-
-    # Временно для ясного текста используется тот же запрос
     input_ids = tokenizer(
-        ["Упрости и перефразируй текст и сохрани основное значение текста: " + text],
+        [text],
         max_length=600,
         add_special_tokens=True,
         padding="max_length",
